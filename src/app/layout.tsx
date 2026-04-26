8aedd013a3d7c6ad430ff2fb3b93a01f73a3ac8e
danilopgon <dani.lopezgonzalez.94@gmail.com>
2026-04-26 13:36:42 +0200

    Actualizar tipografias del sitio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,23 @@ import Footer from '@/components/layout/Footer';
 import LenisWrapper from '@/components/layout/LenisWrapper';
 import Navbar from '@/components/layout/Navbar';
 import type { Metadata } from 'next';
+import { Barlow, Barlow_Condensed } from 'next/font/google';
 import { Toaster } from 'sonner';
 import './globals.css';
+
+const barlow = Barlow({
+  subsets: ['latin'],
+  weight: ['400', '500'],
+  variable: '--font-barlow',
+  display: 'swap',
+});
+
+const barlowCondensed = Barlow_Condensed({
+  subsets: ['latin'],
+  weight: ['600', '700'],
+  variable: '--font-barlow-condensed',
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://hotelsur.es'),
@@ -70,7 +85,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='es'>
+    <html lang='es' className={`${barlow.variable} ${barlowCondensed.variable}`}>
       <body className='antialiased min-h-screen flex flex-col overflow-x-hidden'>
         <script
           type='application/ld+json'
